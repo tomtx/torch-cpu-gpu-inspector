5e336f6fe1c311c68be893792bd1677f8b015ea7
Tomas Turek <32823414+tomtx@users.noreply.github.com>
2018-07-02 17:22:45 -0400

    Update inspector.lua

--- inspector.lua
+++ inspector.lua
@@ -1,17 +1,35 @@
 require 'torch'
+require 'socket'
 
--- create a 2-D tensor of [2, 3] shape
-a = torch.Tensor({{1.0,2.0,3.0},{4.0,5.0,6.0}})
--- create a 2-D tensor of [3, 2] shape
-b = torch.Tensor({{1.0,2.0},{3.0,4.0},{5.0,6.0}})
--- perform a matrix product of those two tensors
-c = torch.mm(a, b)
+function test_cpu_gpu_computations()
+    print("\n++++++++++++++++++++++++++++++")
+    print("... placing matrix operations on available CPU/GPU devices")
+    -- create a 2-D tensor of [2, 3] shape
+    print("Tensor A")
+    a = torch.Tensor({{1.0,2.0,3.0},{4.0,5.0,6.0}})
+    print(a)
+    -- create a 2-D tensor of [3, 2] shape
+    print("Tensor B")
+    b = torch.Tensor({{1.0,2.0},{3.0,4.0},{5.0,6.0}})
+    print(b)
+    -- perform a matrix product of those two tensors
+    print("Matrix Product of Tensors A & B")
+    c = torch.mm(a, b)
+    print(c)
+    print("++++++++++++++++++++++++++++++")
+    return c
+end
 
-print('tensor a')
-print(a)
+function main()
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("+++ Simple CPU/GPU Computation Test with Torch +++")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("start:", os.date("%m/%d/%Y %I:%M %p"), socket.gettime()*1000, "ms")
+    -- verify Torch installation
+    test_cpu_gpu_computations()
+    -- test CPU/GPU computations
+    print("\nend:", os.date("%m/%d/%Y %I:%M %p"), socket.gettime()*1000, "ms")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+end
 
-print('tensor b')
-print(b)
-
-print('tensor c')
-print(c)
+main()
