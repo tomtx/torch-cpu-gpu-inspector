4eb7066c5818f4e8ac1a5549cfe42331d1b98907
Tomas Turek <32823414+tomtx@users.noreply.github.com>
2018-07-02 16:17:47 -0400

    Update inspector.lua

--- inspector.lua
+++ inspector.lua
@@ -1,9 +1,10 @@
 require 'torch'
 
--- a simple text
+-- create a 2-D tensor of [2, 3] shape
 a = torch.Tensor({{1.0,2.0,3.0},{4.0,5.0,6.0}})
+-- create a 2-D tensor of [3, 2] shape
 b = torch.Tensor({{1.0,2.0},{3.0,4.0},{5.0,6.0}})
--- tensor as matrix product between tensor a and b
+-- perform a matrix product of those two tensors
 c = torch.mm(a, b)
 
 print('tensor a')
