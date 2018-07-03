d8ab54a021785057485471f5381be95641404c7a
Tomas Turek <32823414+tomtx@users.noreply.github.com>
2018-07-03 19:07:08 -0400

    Update inspector.lua

--- inspector.lua
+++ inspector.lua
@@ -1,23 +1,71 @@
 require 'torch'
 require 'socket'
 
-function test_cpu_gpu_computations()
+function is_module_available(name)
+  if package.loaded[name] then
+    return true
+  else
+    for _, searcher in ipairs(package.searchers or package.loaders) do
+      local loader = searcher(name)
+      if type(loader) == 'function' then
+        package.preload[name] = loader
+        return true
+      end
+    end
+    return false
+  end
+end
+
+function perform_cpu_computations()
     print("\n++++++++++++++++++++++++++++++")
-    print("... placing matrix operations on available CPU/GPU devices")
+    print("... placing matrix operations on available CPU devices")
     -- create a 2-D tensor of [2, 3] shape
-    print("Tensor A")
+    print("CPU Tensor A")
     a = torch.Tensor({{1.0,2.0,3.0},{4.0,5.0,6.0}})
     print(a)
     -- create a 2-D tensor of [3, 2] shape
-    print("Tensor B")
+    print("CPU Tensor B")
     b = torch.Tensor({{1.0,2.0},{3.0,4.0},{5.0,6.0}})
     print(b)
     -- perform a matrix product of those two tensors
-    print("Matrix Product of Tensors A & B")
+    print("Matrix Product of CPU Tensors A & B")
     c = torch.mm(a, b)
     print(c)
     print("++++++++++++++++++++++++++++++")
-    return c
+end
+
+function perform_gpu_computations()
+    print("\n++++++++++++++++++++++++++++++")
+    print("... placing matrix operations on available GPU devices")
+    cutorch = require 'cutorch'
+    -- create a 2-D tensor of [2, 3] shape
+    print("GPU Tensor A")
+    a = torch.CudaTensor({{1.0,2.0,3.0},{4.0,5.0,6.0}})
+    print(a)
+    -- create a 2-D tensor of [3, 2] shape
+    print("GPU Tensor B")
+    b = torch.CudaTensor({{1.0,2.0},{3.0,4.0},{5.0,6.0}})
+    print(b)
+    -- perform a matrix product of those two tensors
+    print("Matrix Product of GPU Tensors A & B")
+    c = torch.mm(a, b)
+    print(c)
+    print("++++++++++++++++++++++++++++++")
+end
+
+function test_cpu_gpu_computations()
+    -- Perform CPU computations
+    perform_cpu_computations()
+    -- Perform GPU computations
+    -- check if the GPU libraries are present
+    gpu_libraries = is_module_available('cutorch')
+    if gpu_libraries then
+        perform_gpu_computations()
+    else
+        print("\n++++++++++++++++++++++++++++++")
+        print("... GPU devices or libraries are not present!")
+        print("++++++++++++++++++++++++++++++")
+    end
 end
 
 function main()
@@ -26,8 +74,9 @@ function main()
     print("++++++++++++++++++++++++++++++++++++++++++++++++++")
     print("start:", os.date("%m/%d/%Y %I:%M %p"), socket.gettime()*1000, "ms")
     -- verify Torch installation
-    test_cpu_gpu_computations()
+    --verify_torch_installation()
     -- test CPU/GPU computations
+    test_cpu_gpu_computations()
     print("\nend:", os.date("%m/%d/%Y %I:%M %p"), socket.gettime()*1000, "ms")
     print("++++++++++++++++++++++++++++++++++++++++++++++++++")
 end
